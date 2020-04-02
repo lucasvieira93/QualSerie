30b3d0536e40e23ffa5e0275a0c50c0d2a8f711b
Lucas Vieira <lvieirastos@gmail.com>
2020-04-01 23:14:17 -0300

    Código Finalizado

--- app/src/main/java/com/lucasvieira93/qualserie/MainActivity.java
+++ app/src/main/java/com/lucasvieira93/qualserie/MainActivity.java
@@ -3,12 +3,47 @@ package com.lucasvieira93.qualserie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private ImageView imagemExibicao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekBarId);
+        imagemExibicao = findViewById(R.id.imagemExibicaoId);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int valorProgresso = progress;
+
+                if (valorProgresso == 0){
+                    imagemExibicao.setImageResource(R.drawable.pouco);
+                } else if (valorProgresso == 1) {
+                    imagemExibicao.setImageResource(R.drawable.medio);
+                } else if (valorProgresso == 2) {
+                    imagemExibicao.setImageResource(R.drawable.muito);
+                } else if (valorProgresso == 3) {
+                    imagemExibicao.setImageResource(R.drawable.susto);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
